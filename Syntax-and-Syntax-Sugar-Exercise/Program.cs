b5603a25f7b2cebf3f0c98849eaa8b7f857aceed
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-08-26 08:56:55 -0700

    All Done

---
+++ Syntax-and-Syntax-Sugar-Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Given the following code, change the code to implement:
+
+inferred typing
+string interpolation
+and the ternary operator
+    */
+/*
+int answer = 4;
+string response;
+if (answer < 9)
+{
+    response = answer + " is less than nine";
+}
+else
+{
+    response = answer + " is greater than or equal to nine";
+}
+Console.WriteLine(response);
+*/
+// Solution
+
+
+//ternary operator with inferred typing
+var answer = 4;
+string response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+Console.WriteLine(response);
+
